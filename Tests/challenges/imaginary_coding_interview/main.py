0bb18d291d3356ed8ba434feabf6ec5bd95bc3c0
MarinhoCM <marinhoc384@gmail.com>
2023-08-29 18:41:17 -0300

    :tada: feat: new challenge solved

--- Tests/challenges/imaginary_coding_interview/main.py
+++ Tests/challenges/imaginary_coding_interview/main.py
@@ -1,7 +1,8 @@
 from typing import List
 
+
 class Interview:
-    def __init__(self, time_list: List[int], max_time: int) -> None:
+    def __init__(self: object, time_list: List[int], max_time: int) -> None:
         self.__time_list = time_list
         self.__max_time = max_time
     
@@ -13,13 +14,32 @@ class Interview:
     def max_time(self) -> int:
         return self.__max_time
     
+    
+    def verify_limit_spent_in_question(self: object) -> bool:
+        times = self.time_list
+        if (times[0] > 5) or (times[1] > 5):
+            return False
+        elif (times[2] > 10) or (times[3] > 10):
+            return False
+        elif (times[4] > 15) or (times[5] > 15):
+            return False
+        elif (times[6] > 20 ) or (times[7] > 20):
+            return False
+        else:
+            return True
+    
+    
     def interview(self) -> str:
-        for i in self.time_list:
-            if i > 20:
-                return "disqualified"
-            else:
-                result = sum(self.time_list, 0)
-                if result <= self.max_time:
-                    return "qualified"
+        if len(self.time_list) == 8 and self.max_time <= 120:
+            if self.verify_limit_spent_in_question():
+                result: int = sum(self.time_list, 0)
+                result_with_tolerance_time: int = int(result - 20)
+                if (result_with_tolerance_time) <= self.max_time:
+                    return "qualified" 
                 else:
-                    return "disqualified"
+                    return "disqualified"    
+            else:
+                return "disqualified"
+        else:
+            return "disqualified"
+            
